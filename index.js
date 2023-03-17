2dd64e3e6a3d2ecfcf38f6d28a680496106ff852
luismca18 <luismca18@gmail.com>
2023-03-17 00:34:41 -0600

    input

--- index.js
+++ index.js
@@ -1,42 +1,7 @@
-const {ApolloServer,gql}=require('apollo-server');
+const {ApolloServer} = require(`apollo-server`);
+const typeDefs=require(`./db/schema`);
+const resolvers=require(`./db/resolvers`);
 
-//Schema
-const typeDefs=gql`
-    type Curso{
-        titulo: String
-        tecnologia: String
-    }
-
-    type Query{
-        obtenerCursos : Curso
-    }
-`;
-
-const cursos = [
-    {
-        titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
-        tecnologia: 'JavaScript ES6',
-    },
-    {
-        titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
-        tecnologia: 'React',
-    },
-    {
-        titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
-        tecnologia: 'Node.js'
-    }, 
-    {
-        titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
-        tecnologia: 'React'
-    }
-];
-
-//resolvers
-const resolvers={
-    Query:{
-       obtenerCursos : ()=> cursos[0]
-    }
-}
 
 //Servidor
 const server = new ApolloServer({
@@ -46,5 +11,5 @@ const server = new ApolloServer({
 
 //arrancar el servidor
 server.listen().then(({url})=>{
-    console.log('Servidor listo en la URL ${url}')
+    console.log(`Servidor listo en la URL ${url}`)
 });
